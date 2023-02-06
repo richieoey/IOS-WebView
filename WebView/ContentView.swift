e1f5cca85144f0879690bdefe110b000cdd796ee
richieoey <richie.oey88@gmail.com>
2023-02-06 14:08:05 +0700

    Initial Commit

---
+++ WebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebView
+//
+//  Created by Richie Darmawan Oey on 06/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Richie Darmawan Oey on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
